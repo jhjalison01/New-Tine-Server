10903adbeb966f461b147a45362831799cab774b
ChaeyeonHan <71596178+ChaeyeonHan@users.noreply.github.com>
2023-08-15 21:56:29 +0900

    Revert "Feature/#17 set habit"

--- src/main/java/com/umc/NewTine/NewTineApplication.java
+++ src/main/java/com/umc/NewTine/NewTineApplication.java
@@ -2,20 +2,10 @@ package com.umc.NewTine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
-
-@EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class NewTineApplication {
-
-	@PostConstruct
-	public void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewTineApplication.class, args);
